c6f2e82a65adab820eac8d6d9c08ab8955c3ec88
astefanutti <antonin.stefanutti@murex.com>
2014-10-21 18:43:04 +0200

    Add SVG viewBox and support zoom and drag

--- webapp/src/main/webapp/cdeye.js
+++ webapp/src/main/webapp/cdeye.js
@@ -20,7 +20,7 @@ function display() {
 //    div.innerHTML = "";
     for (var i = 0; i < beans.bean.length; i++) {
 //        div.innerHTML += beans.bean[i].className + "<br/>";
-        nodes.push({name:beans.bean[i].classSimpleName, width:400, height:40});
+        nodes.push({name:beans.bean[i].classSimpleName, width:200, height:40});
     }
 
     console.info(nodes);
@@ -29,19 +29,33 @@ function display() {
 //        console.info(graph.nodes);
 //    });
 
-    var width = 800, height = 800;
-
     var color = d3.scale.category20();
 
     var d3cola = cola.d3adaptor()
         .linkDistance(100)
         .avoidOverlaps(true)
         .handleDisconnected(false)
-        .size([width, height]);
+        .size([800, 800]);
 
     var svg = d3.select("body").append("svg")
-        .attr("width", width)
-        .attr("height", height);
+        .attr("width", "100%")
+        .attr("height", "100%")
+        .attr("preserveAspectRatio", "xMidYMid")
+        .attr("viewBox", "0 0 " + (800) + " " + (800))
+        .attr("pointer-events", "all");
+        //.call(d3.behavior.zoom().on("zoom", redraw));
+
+    svg.append('rect')
+        .attr('class', 'background')
+        .attr('width', "100%")
+        .attr('height', "100%")
+        .call(d3.behavior.zoom().on("zoom", redraw));
+
+    var container = svg.append('g');
+
+    function redraw() {
+        container.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
+    }
 
     d3cola
         .nodes(nodes)
@@ -61,44 +75,54 @@ function display() {
 //            .enter().append("line")
 //            .attr("class", "link");
 
-    var pad = 3;
-    var node = svg.selectAll(".node")
+    var margin = 10, pad = 12;
+
+    var node = container.selectAll(".node")
         .data(nodes)
         .enter().append("rect")
         .attr("class", "node")
         .attr("width", function (d) { return d.width - 2 * pad; })
         .attr("height", function (d) { return d.height - 2 * pad; })
         .attr("rx", 5).attr("ry", 5)
-        .style("fill", function (d) { return color(/*graph.groups.length*/ 3); })
+        .style("fill", function (d) { return color( /*graph.groups.length*/ 3); })
         .call(d3cola.drag);
 
-    var label = svg.selectAll(".label")
+    var label = container.selectAll(".label")
         .data(nodes)
         .enter().append("text")
         .attr("class", "label")
         .text(function (d) { return d.name; })
-        .call(d3cola.drag);
+        .call(d3cola.drag)
+        .each(function (d) {
+            var b = this.getBBox();
+            var extra = 2 * margin + 2 * pad;
+            d.width = b.width + extra;
+            d.height = b.height + extra;
+        });
 
-    node.append("title")
-        .text(function (d) { return d.name; });
+    //node.append("title")
+    //    .text(function (d) { return d.name; });
 
     d3cola.on("tick", function () {
 //        link.attr("x1", function (d) { return d.source.x; })
 //            .attr("y1", function (d) { return d.source.y; })
 //            .attr("x2", function (d) { return d.target.x; })
 //            .attr("y2", function (d) { return d.target.y; });
-        node.attr("x", function (d) { return d.x - d.width / 2 + pad; })
-            .attr("y", function (d) { return d.y - d.height / 2 + pad; });
+        node.each(function (d) { d.innerBounds = d.bounds.inflate(-margin); })
+            .attr("x", function (d) { return d.innerBounds.x; })
+            .attr("y", function (d) { return d.innerBounds.y; })
+            .attr("width", function (d) { return d.innerBounds.width(); })
+            .attr("height", function (d) { return d.innerBounds.height(); });
+        //node.attr("x", function (d) { return d.x - d.width / 2 + pad; })
+        //    .attr("y", function (d) { return d.y - d.height / 2 + pad; })
+//            .attr("width", function (d) { return d.width; })
+//            .attr("height", function (d) { return d.height; });
 //        group.attr("x", function (d) { return d.bounds.x; })
 //            .attr("y", function (d) { return d.bounds.y; })
 //            .attr("width", function (d) { return d.bounds.width(); })
 //            .attr("height", function (d) { return d.bounds.height(); });
         label.attr("x", function (d) { return d.x; })
-            .attr("y", function (d) {
-                var h = this.getBBox().height;
-                return d.y + h/4;
-            });
+            //.attr("y", function (d) { var h = this.getBBox().height;return d.y + h/4;});
+        .attr("y", function (d) { return d.y + (margin + pad) / 2 });
     });
-
-//    });
 }
